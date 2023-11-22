942aee226b23d262ce99d289632ffff3d29736a4
Zachary <zdean1995@gmail.com>
2023-11-21 20:33:32 -0500

    Add project files.

---
+++ AskUrzaPlus/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AskUrzaPlus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AskUrzaPlus/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AskUrzaPlus
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
